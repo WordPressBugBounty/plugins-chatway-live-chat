f3fac714d1c4197b4994f38e3266a60308140049
Sean Murphy <sean@iamseanmurphy.com>
2026-02-11 04:21:01 +0000

    1.4.5

--- chatway-live-chat/assets/js/app.asset.php
+++ chatway-live-chat/assets/js/app.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-hooks'), 'version' => '517aaadcb606366b0c22');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-element', 'wp-hooks'), 'version' => '11951cef5c025abcf8e0');
